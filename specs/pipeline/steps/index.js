7ab198b4b542c6b85927cf1b4e4bb86aedff4d7c
Laurent Decorps <ldecorps@gmail.com>
2026-08-20 00:51:46 +0100

    Revert "Merge commit '1498580a17' into swarmforge-architect"
    This reverts commit 5b84c3bc3623f58a4d88490c0ad901a62e5f7100, reversing
    changes made to f532c6def2e086cad62098aef94ee238a0cee7cb.

--- specs/pipeline/steps/index.js
+++ specs/pipeline/steps/index.js
@@ -520,8 +520,7 @@ const DOMAINS = [
   require('./bl571SequentialRotationDormantParitySteps'),
   require('./bl958ControlPlaneLossSteps'),
   require('./bl960HealWrapperParseSafetySteps'),
-  require('./bl957DependsOnPromotionGateSteps'),
-  require('./bl959ApsEquivalenceSteps')
+  require('./bl957DependsOnPromotionGateSteps')
 ];
 
 function registerSteps(registry) {
