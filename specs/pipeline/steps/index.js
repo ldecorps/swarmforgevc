72bf22b38a005e94b8d42224e8760c4efd7c6b32
t <t@t>
2026-08-27 13:21:44 +0100

    docs(BL-1166): restore Last Updated + architecture note; documenter rematch evidence; dedupe step registration. By documenter.

--- specs/pipeline/steps/index.js
+++ specs/pipeline/steps/index.js
@@ -789,7 +789,6 @@ const DOMAINS = [
   require('./bl1120HandoffdMustNotAbortForeignMergeSteps'),
   require('./bl683HandoffDepthWarningCountsNonTicketsSteps'),
   require('./bl780NoteActionabilityOutrunsWatchdogWarnSteps'),
-  require('./bl1166OperatorDocsSteps'),
   require('./bl1185WorkNoteMissingTaskHeaderSteps'),
   require('./bl1167SameModelSeatRoutingSteps'),
   require('./bl1175PropertySuiteStandingRedsSteps'),
