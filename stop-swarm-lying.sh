08d0e7bbe9125b6a63e00c668859a33032f8ed01
t <t@t>
2026-08-27 19:53:21 +0100

    init

--- stop-swarm-lying.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p .swarmforge/expedite-fixture
-printf '%s\n' "$0 $*" >> .swarmforge/expedite-fixture/stop-invocations.log
-echo "kill_all_swarm SUCCESS - clean slate"
-exit 0
