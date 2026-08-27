617719bf498d5affc53d932ffe6295b45f91ba30
t <t@t>
2026-08-27 13:48:09 +0100

    chore(BL-1171): materialize disaster-class correlation wiring. By documenter.

--- specs/pipeline/steps/index.js
+++ specs/pipeline/steps/index.js
@@ -662,6 +662,7 @@ const DOMAINS = [
   require('./bl586PipelineBoardTopicIdentitySteps'),
   require('./bl1017BabysitterdRecreatesVanishedStandingSessionSteps'),
   require('./bl1169BabysitterHalfLaunchStarvationAutoRepairSteps'),
+  require('./bl1171DisasterClassCorrelationStructuredEscalationSteps'),
   require('./bl1163HandoffdParseErrorBl668ParenHotfixSteps'),
   require('./bl1164QaChangedPathUnitTestGateSteps'),
   require('./bl1173DeprecatorFreshnessGateCliSteps'),
